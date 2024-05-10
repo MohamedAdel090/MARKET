ec68c37121ac501ff6ba29d6499a74466e5a6aeb
Muhammed Adel <mohamedadel@gmail.com>
2024-05-10 14:06:10 +0300

    my new project

--- src/app/products/components/all-products/all-products.component.ts
+++ src/app/products/components/all-products/all-products.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-all-products',
   templateUrl: './all-products.component.html',
   styleUrl: './all-products.component.scss'
 })
-export class AllProductsComponent {
+export class AllProductsComponent implements OnInit {
 
+  allProducts:any[] = []
+
+    constructor(private _ProductsService:ProductsService){}
+
+
+ngOnInit(): void {
+  this.getProduct()
+}
+
+
+  getProduct(){
+    this._ProductsService.getAllProducts().subscribe((res:any) =>{
+      console.log(res)
+    })
+  }
 }
