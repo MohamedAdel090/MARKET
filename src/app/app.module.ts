b58e815dbf379773b99226c635e0f98f9fcce2fb
Muhammed Adel <mohamedadel@gmail.com>
2024-05-24 23:31:37 +0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,25 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+import * as sharedModule from './shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { AllProductsComponent } from './products/components/all-products/all-products.component';
+import { SharedModule as SharedModule } from "./shared/shared.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AllProductsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CommonModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AllProductsComponent
+    ],
+    providers: [
+        provideClientHydration()
+    ],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        CommonModule,
+        sharedModule.SharedModule
+    ]
 })
 export class AppModule { }
